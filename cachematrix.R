0a2cd0ab638ffa867b0909451328d53c070adb2d
aadobson <aadobson@ust.hk>
2015-03-17 11:58:22 -1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## -----------------------------------------------------------------------------------
+## The functions in this file allow the creation of R objects ("cache matrices") that
+## encapsulate a numeric matrix and its inverse.  The matrix itself and the inverse
+## are cached in memory so that the inverse only needs to be computed once.
+## -----------------------------------------------------------------------------------
 
-## Write a short comment describing this function
-
+## -----------------------------------------------------------------------------------
+## makeCacheMatrix() takes a single argument, a matrix, and returns a list of named
+##      functions defined in the body of the function.  These functions are used to
+##      redefine and retieve the matrix (set and get) and to specify and retrieve
+##      the cached inverse (setinv and getinv)
+## -----------------------------------------------------------------------------------
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
+        ## The function set(y) assigns the new value y to the label x
+        ## and also resets i to NULL to require the inverse to be
+        ## recalculated when cacheSolve() is called.
         set <- function(y) {
                 x <<- y  
                 i <<- NULL
@@ -12,20 +22,30 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         setinv <- function(solv) i <<- solv
         getinv <- function() i
+        ## The list below is returned by makeCacheMatrix.  It contains a series of
+        ## elements named "set", "get", etc, whose values are pointers to the
+        ## functions set(), get(), etc
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## -----------------------------------------------------------------------------------
+## cacheSolve plays the role for cache matrices that solve() plays for regular
+##      matrices; namely, it returns the matrix inverse. If a value has already
+##      been cached, it retrieves and returns this value; otherwise, it uses solve()
+##      to compute the inverse and caches this before returning it.  Note that any
+##      additional arguments denoted by ... are passed to solve() if this is called.
+## -----------------------------------------------------------------------------------
 cacheSolve <- function(M, ...) {
+        ## Gets the inverse, if it has been cached; otherwise gets NULL
         solv <- M$getinv()
+        ## If solv is not NULL, it is the cached value and is returned
         if(!is.null(solv)) {
                 message("getting cached data")
                 return(solv)
         }
+        ## Otherwise, the matrix is retrieved and its inverse is calculated
         data <- M$get()
         solv <- solve(data, ...)
         M$setinv(solv)
